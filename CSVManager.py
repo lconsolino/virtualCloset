21012d0ab7de2a1b39cc59d4e71e9aff10626800
Lizzie Consolino <lconsoli@usc.edu>
2025-12-10 00:08:05 -0800

    initial commit

---
+++ CSVManager.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+from threading import Lock
+import importCSV
+import csv
+from pprint import pprint
+
+MAIL_REQ_FIELDS = ['subject', 'body', 'sender']
+MAIL_DB_FILE = 'mail.pickle'
+class clothingManager(object):
+    def __init__(self):
+        """
+        Summary: Class for managing the dictionary of mail data and the pickle
+                file used for persistent storage
+        """
+
+        print('starting manager')
+
+        headers = ['id','gender','masterCategory','subCategory','articleType','baseColour','season','year','usage','productDisplayName']
+
+        clothingFile = "styles.csv"
+
+        columns =[]
+        rows =[]
+        dictionaryArr= []
+        with open(clothingFile, 'r') as csvfile:
+            readClothing= csv.reader(csvfile)
+            columns=next(readClothing)
+            for item in readClothing:
+                rows.append(item)
+        for row in rows[1:]:
+            # parsing each column of a row
+            lineDictionary={}
+            for i, col in enumerate(row):
+                if(i<len(headers)):
+                    lineDictionary[headers[i]]=col
+            dictionaryArr.append(lineDictionary)
+        self.dictionaryArr=dictionaryArr
+        self.headers=headers
+        
+    def searchObjects(self,key,value):
+        
+        pprint(value)
+        pprint(key)
+        if(key=='' and value ==''):
+            return "no key value"
+        for item in self.headers:
+            pprint(item)
+            if item == key:
+                for object in self.dictionaryArr[1:]:
+                    pprint(object[key])
+                    if value in object[key]:
+                        pprint(object[key] )
+                        return object
+        return "none found"
+
+        # params ={
+        #     key: value
+        # }
+        # response = requests.get("http://{}/item".format(self.serv_addr), params=params)
+    def findResult(self,input):
+        results = []git checkout --theirs requirements.txt templates/result.html templates/searchResult.html templates/test.html
+
+        for category in self.headers:
+            for keyword in self.dictionaryArr[1:]:
+                if (" " + input.lower() + " ") in keyword[category].lower():
+                    pprint(keyword[category])
+                    results.append(keyword)
+                    # return keyword
+        return results
+    def getKeywordItems(self,inputID):
+        results=[]
+        keywords=[]
+        pprint(id)
+        item = next((entry for entry in self.dictionaryArr if entry.get('id') == inputID), None)
+        pprint(item)
+        for category in self.headers:
+            keywords.append(item[category])
+        for category in self.headers:
+            for keyword in self.dictionaryArr[1:]:
+                for attribute in keywords:
+                    if(attribute in keyword[category].lower()):
+                        # pprint(keyword[category])
+                        results.append(keyword)
+                        continue
+        return results
